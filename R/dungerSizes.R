1d468c708cb835449bc6ecc2ee7851d37d432667
Ewelti <mischiefmao@gmail.com>
2023-06-12 14:28:08 -0600

    adding bayesian estimates of total body size

--- R/dungerSizes.R
+++ R/dungerSizes.R
@@ -42,24 +42,26 @@ for(i in unique(cp$trt_mo)){
 } ; rm(i)
 ests_cp
 
-fit <- brm(body.length..mm. ~ 1 + (1|trap:site), data = cp)
-cp_fixed_95 <- fixef(fit, probs = c(0.05, 0.95))[1,]
-
 ests_cp[c('trt', 'month')] <- str_split_fixed(ests_cp$trt_mo, ' ', 2)
 colnames(ests_cp)[2] ="body_mm_est"
 colnames(ests_cp)[3] ="body_mm_SE"
 ests_cp$month <-as.numeric(ests_cp$month)
 ests_cp <- ests_cp[order(ests_cp$month,ests_cp$trt),]
 ests_cp$mo_jit <- (ests_cp$month + c(-0.1,-0.2,0,0.2,0.1))
+ests_cp$spp <- rep("CP", 15)
 
 ##
 ##Onthophagus_nuchicornis_female
-onF$trt_mo <- paste(onF$trt,onF$month)
+onF$trt_mo <- paste(onF$trt,onM$month)
 ests_onF <- NULL
 for(i in unique(onF$trt_mo)){
   sub <- onF[onF$trt_mo == i, ]
-  ests_onF.i <- coef(summary(lmer(body.length..mm. ~ 1 + (1|trap:site), data = sub, )))[1,1:2]
-  ests_onF.i <- data.frame(trt_mo = i, t(ests_onF.i))
+  fit <- brm(body.length..mm. ~ 1 + (1|trap:site), data = sub)
+	#pull out fixed effects
+	onF_fixed_95 <- fixef(fit, probs = c(0.05, 0.95))[1,]
+	onF_fixed_90 <- fixef(fit, probs = c(0.10, 0.90))[1,3:4]
+	onF_fixed_80 <- fixef(fit, probs = c(0.20, 0.80))[1,3:4]
+  ests_onF.i <- data.frame(trt_mo = i, t(onF_fixed_95), t(onF_fixed_90), t(onF_fixed_80))
   ests_onF <- rbind(ests_onF, ests_onF.i) ; rm(ests_onF.i, sub)
 } ; rm(i)
 ests_onF
@@ -70,6 +72,7 @@ colnames(ests_onF)[3] ="body_mm_SE"
 ests_onF$month <-as.numeric(ests_onF$month)
 ests_onF <- ests_onF[order(ests_onF$month,ests_onF$trt),]
 ests_onF$mo_jit <- (ests_onF$month + c(-0.1,-0.2,0,0.2,0.1))
+ests_onF$spp <- rep("ONf", 15)
 
 ##
 ##Onthophagus_nuchicornis_male
@@ -77,8 +80,12 @@ onM$trt_mo <- paste(onM$trt,onM$month)
 ests_onM <- NULL
 for(i in unique(onM$trt_mo)){
   sub <- onM[onM$trt_mo == i, ]
-  ests_onM.i <- coef(summary(lmer(body.length..mm. ~ 1 + (1|trap:site), data = sub, )))[1,1:2]
-  ests_onM.i <- data.frame(trt_mo = i, t(ests_onM.i))
+  fit <- brm(body.length..mm. ~ 1 + (1|trap:site), data = sub)
+	#pull out fixed effects
+	onM_fixed_95 <- fixef(fit, probs = c(0.05, 0.95))[1,]
+	onM_fixed_90 <- fixef(fit, probs = c(0.10, 0.90))[1,3:4]
+	onM_fixed_80 <- fixef(fit, probs = c(0.20, 0.80))[1,3:4]
+  ests_onM.i <- data.frame(trt_mo = i, t(onM_fixed_95), t(onM_fixed_90), t(onM_fixed_80))
   ests_onM <- rbind(ests_onM, ests_onM.i) ; rm(ests_onM.i, sub)
 } ; rm(i)
 ests_onM
@@ -89,6 +96,7 @@ colnames(ests_onM)[3] ="body_mm_SE"
 ests_onM$month <-as.numeric(ests_onM$month)
 ests_onM <- ests_onM[order(ests_onM$month,ests_onM$trt),]
 ests_onM$mo_jit <- (ests_onM$month + c(-0.1,-0.2,0,0.2,0.1))
+ests_onM$spp <- rep("ONm", 15)
 
 ########################################
 #calculate trt effects on pasture meter
